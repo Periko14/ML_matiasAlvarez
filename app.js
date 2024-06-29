7f67930b7fcdf34f84152a0287f87216abd08118
matias123t1@gmail.com <tuemail@example.com>
2024-06-29 10:43:14 -0300

    Agregando test y enviando

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require ("express");
 const path = require('path');
 const app = express();
 
-const port = 3030;
+const port = process.env.PORT || 3030;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'views')));
